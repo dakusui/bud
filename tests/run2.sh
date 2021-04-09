f67f60571fd4d3799488075724441546ec97ad25
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2021-04-10 02:48:37 +0900

    Implement cmd_unit (WIP)

--- tests/run2.sh
+++ tests/run2.sh
@@ -1,11 +1,6 @@
 source "$(dirname "$(dirname "${BASH_SOURCE[0]}")")/lib/bud.rc"
 
-set -eu -o pipefail
-shopt -s inherit_errexit nullglob compat"${BASH_COMPAT=42}"
+export BUD_DEBUG=enabled
 
-export BUD_DEBUG=disabled
-
-message "hi1"
 _test_json="$(jq -rcM . "$(dirname "${BASH_SOURCE[0]}")"/example.json)"
-message "hi2"
 execute_test_json "${_test_json}"
