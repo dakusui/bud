d87033305c75d06d2d9e2822064bce1543f837a6
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2021-04-14 22:14:07 +0900

    Implement 'testrunner' (WIP)

--- src/test/scripts/test-passing.sh
+++ src/test/scripts/test-passing.sh
@@ -4,4 +4,4 @@ source "${LIBDIR}/bud.rc"
 export BUD_DEBUG=enabled
 
 _test_json="$(jq -rcM . "$(dirname ${BASH_SOURCE[0]})/data/passing.json")"
-__bud__execute_test_json "${_test_json}"
+__bud__execute_test_json "${_test_json}" | jq .
