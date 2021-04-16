0b0c3b66e7eb8aad24e6d850817ac696f8b261c9
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2021-04-17 07:28:35 +0900

    Implement 'testrunner' - test claen up / yaml support

--- src/test/scripts/test-precheck-and-execute.sh
+++
@@ -1,6 +0,0 @@
-source "${BUD_LIB}/bud.rc"
-
-export BUD_DEBUG=enabled
-
-_test_json="$(jq -rcM . "$(dirname "${BASH_SOURCE[0]}")"/data/passing.json)"
-run_test_json "${_test_json}" | jq .
--- src/test/scripts/test-precheck-and-failed.sh
+++ src/test/scripts/test-precheck-and-failed.sh
@@ -7,14 +7,14 @@ _tmp_dir=$(mktemp -d)
 function bud__directory_for_output() {
   echo "${_tmp_dir}"
 }
-_test_json="$(jq -rcM . "$(dirname "${BASH_SOURCE[0]}")"/data/ignored.json)"
+_test_json="$(jq -rcM . "$(dirname "${BASH_SOURCE[0]}")"/data/failing.json)"
 
 echo "RUN TEST" >&2
 _result_json="$(run_test_json "${_test_json}")"
 
 echo "CHECK TEST RESULT(1)" >&2
 _precheck=$(echo "${_result_json}" | jq -crM .report.result.precheck)
-if [[ "${_precheck}" == false ]]; then
+if [[ "${_precheck}" == true ]]; then
   exit 0
 else
   echo "_precheck: ${_precheck}" >&2
@@ -23,10 +23,11 @@ else
 fi
 echo "CHECK TEST RESULT(2)" >&2
 _success=$(echo "${_result_json}" | jq -crM .report.result.success)
-if [[ "${_success}" == null ]]; then
+if [[ "${_success}" == false ]]; then
   exit 0
 else
   echo "_success: ${_success}" >&2
-  echo "_result_json: '${_result_json}'" exit 1 >&2
+  echo "_result_json: '${_result_json}'" >&2
+  exit 1
 fi
 echo "DONE" >&2
